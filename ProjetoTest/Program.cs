d6269b85b1eccf2906d47bba41643cc26a75ffc8
Francisco <francisco.amartins.al@gmail.com>
2019-10-18 13:51:15 -0300

    Adicionando um simbolo

--- ProjetoTest/Program.cs
+++ ProjetoTest/Program.cs
@@ -8,46 +8,47 @@ namespace ProjetoTest
         static void Main(string[] args)
         {
             int resp = 0;
-            do
-            {
-                Console.Write("Olá, Informe o nome do aluno(a) : ");
-                string nome = Console.ReadLine();
-
-                Console.WriteLine(" ---- Informe as notas do aluno ----");
-
-                Console.Write("Nota 1ª: ");
-                double n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-                Console.Write("Nota 2ª: ");
-                double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-                Console.Write("Nota 3ª: ");
-                double n3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-                double media = (n1 + n2 + n3) / 3;
-
-                Console.WriteLine($"A média entre as notas de {nome} é : [{media.ToString("F2", CultureInfo.InvariantCulture)}]");
-
-                if (media == 5)
+  
+                do
                 {
-                    Console.WriteLine("----Aluno está de recuperação----");
-                }
-                else if (media < 5)
-                {
-                    Console.WriteLine("----Aluno está reprovado----");
-                }
-                else if (media > 5)
-                {
-                    Console.WriteLine("----Aluno aprovado----");
-                }
+                    Console.Write("Olá, Informe o nome do aluno(a) : ");
+                    string nome = Console.ReadLine();
 
-                DateTime t1 = DateTime.Now;
-                Console.WriteLine($" Hórario do Cálculo (({t1}))");
+                    Console.WriteLine(" ---- Informe as notas do aluno ----");
 
-                Console.Write("Deseja cálcular a média de um aluno ? 1 = Sim / 0 = Não : ");
-                resp = int.Parse(Console.ReadLine());
+                    Console.Write("Nota 1ª: ");
+                    double n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            } while (resp == 1);
+                    Console.Write("Nota 2ª: ");
+                    double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    Console.Write("Nota 3ª: ");
+                    double n3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    double media = (n1 + n2 + n3) / 3;
+
+                    Console.WriteLine($"A média entre as notas de {nome} é : [{media.ToString("F2", CultureInfo.InvariantCulture)}]");
+
+                    if (media == 5)
+                    {
+                        Console.WriteLine("<----Aluno está de recuperação---->");
+                    }
+                    else if (media < 5)
+                    {
+                        Console.WriteLine("<----Aluno está reprovado---->");
+                    }
+                    else if (media > 5)
+                    {
+                        Console.WriteLine("<----Aluno aprovado---->");
+                    }
+
+                    DateTime t1 = DateTime.Now;
+                    Console.WriteLine($" Hórario do Cálculo (({t1}))");
+
+                    Console.Write("Deseja cálcular a média de um aluno ? 1 = Sim / 0 = Não : ");
+                    resp = int.Parse(Console.ReadLine());
+
+                } while (resp == 1);
         }
     }
 }
