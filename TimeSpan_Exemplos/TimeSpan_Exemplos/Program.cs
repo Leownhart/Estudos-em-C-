b9b2621400570e6a87ed01d231948d792c18f6de
Francisco <francisco.amartins.al@gmail.com>
2019-10-24 13:40:51 -0300

    TimeSpan

---
+++ TimeSpan_Exemplos/TimeSpan_Exemplos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TimeSpan_Exemplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Um objeto TimeSpan internamente armazena uma duração na forma de ticks(100 nanosegundos)*/
+
+            TimeSpan t1 = new TimeSpan(0, 1, 30);
+            Console.WriteLine(t1);
+            Console.WriteLine(t1.Ticks);
+        }
+    }
+}
