0616e8a245ae72c3ba15433c73923c9faa66e799
Francisco <francisco.amartins.al@gmail.com>
2019-10-24 15:05:12 -0300

    Continuação

---
+++ Op_DateTime/Op_DateTime/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Op_DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d = new DateTime(2001, 8, 15, 13, 45, 58);
+            Console.WriteLine(d.ToLongDateString());
+            Console.WriteLine(d.ToLongTimeString());
+            Console.WriteLine(d.ToShortDateString());
+            Console.WriteLine(d.ToShortTimeString());
+
+            string d1 = d.ToString();
+            Console.WriteLine(d1);
+
+            string d2 = d.ToString("yyyy-MM-dd HH:mm:ss");
+            Console.WriteLine(d2);
+
+            string d3 = d.ToString("yyyy-MM-dd HH:mm:ss.fff");
+            Console.WriteLine(d3);
+
+            /*
+            Console.WriteLine(d);
+            Console.WriteLine("1) Date: " + d.Date);
+            Console.WriteLine("2) Day: " + d.Day );
+            Console.WriteLine("3) DayOfWeek: " + d.DayOfWeek);
+            Console.WriteLine("4) DayOfYear: " + d.DayOfYear);
+            Console.WriteLine("5) Hour: " + d.Hour);
+            Console.WriteLine("6) Kind: " + d.Kind);
+            Console.WriteLine("7) Millisecond: " + d.Millisecond);
+            Console.WriteLine("8) Minute: " + d.Minute);
+            Console.WriteLine("9) Month: " + d.Month);
+            Console.WriteLine("10) Second: " + d.Second);
+            Console.WriteLine("11) Ticks: " + d.Ticks);
+            Console.WriteLine("12) TimeOfDay: " + d.TimeOfDay);
+            Console.WriteLine("13) Year: " + d.Year); */
+
+        }
+    }
+}
