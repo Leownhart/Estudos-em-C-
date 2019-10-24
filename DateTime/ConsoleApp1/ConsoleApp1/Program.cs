ce72c5373fc42056110ea83e734a12304c2745e0
Francisco <francisco.amartins.al@gmail.com>
2019-10-24 12:54:55 -0300

    Atualizando Repósitorio

---
+++ DateTime/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = DateTime.Now;
+            Console.WriteLine(d1);
+            Console.WriteLine(d1.Ticks);
+        }
+    }
+}
