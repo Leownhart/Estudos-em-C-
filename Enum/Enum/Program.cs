616200453ad5fb72fadeafca764b6d82d4991f48
Francisco <francisco.amartins.al@gmail.com>
2019-10-25 15:01:54 -0300

    Enum

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,28 @@
+﻿using Enum.Entities.Enums;
+using Enum.Entities;
+using System;
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PedingPayment
+            };
+            Console.WriteLine(order);
+
+            // Conversão de um tipo enumerado para string
+            string txt = OrderStatus.PedingPayment.ToString();
+            Console.WriteLine(txt); 
+
+            // Conversão de um tipo string para enumerado
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+        }
+    }
+}
