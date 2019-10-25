ec7168712c935210286c67fe7165ecc6f78d4566
Francisco <francisco.amartins.al@gmail.com>
2019-10-25 00:19:17 -0300

    Padrão  iso_8601

---
+++ Padrao_ISO_8601/Padrao_ISO_8601/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Padrao_ISO_8601
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = DateTime.Parse("2000-08-15");
+            DateTime d2 = DateTime.Parse("200-08-15T13:05:58Z");
+
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+        }
+    }
+}
