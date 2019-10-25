2f81a3f690aa2a94478d0a71b6560dc7e6f2ca5d
Francisco <francisco.amartins.al@gmail.com>
2019-10-24 23:23:29 -0300

    Op_TimeSpan

---
+++ Op_TimeSpan/Op_TimeSpan/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Op_TimeSpan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan t = new TimeSpan(2, 3, 5, 7, 11);
+            Console.WriteLine(t);
+
+            Console.WriteLine($"Days: {t}");
+
+         /* TimeSpan t1 = TimeSpan.MaxValue;
+            TimeSpan t2 = TimeSpan.MinValue;
+            TimeSpan t3 = TimeSpan.Zero;
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3); */
+        }
+    }
+}
