89f33ddfb4ab9c5622e7b551cefc2f87a13950ef
Francisco <francisco.amartins.al@gmail.com>
2019-10-29 00:58:38 -0300

    Ucpag

---
+++ Course/Course/Entities/Department.cs
@@ -0,0 +1,15 @@
+﻿namespace Course.Entities
+{
+    class Department // Classe Department
+    {
+        public string Name { get; set; }
+        public Department()
+        {
+            // Construtor Padrão.
+        }
+        public Department (string Name)
+        {
+            this.Name = Name; // Contrutor Recebendo  Name.
+        }
+    }
+}
