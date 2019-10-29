553ffb99f42d6b598a162a4ebb4ac544a160d280
Francisco <francisco.amartins.al@gmail.com>
2019-10-29 02:07:45 -0300

    Desenvolvendo

---
+++ UcPag/UcPag/Payment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UcPag
+{
+    class Payment
+    {
+        public decimal CaptalInicial { get; set; }
+        public int Periodo { get; set; }
+
+        public Payment (decimal CaptalInicial, int Periodo)
+        {
+            this.CaptalInicial = CaptalInicial;
+            this.Periodo = Periodo;
+        }
+        public Payment()
+        {
+
+        }
+        public double GerarTaxaJuro(double taxa)
+        {
+            return 0.01;
+        }
+        public double CalcularJuros(decimal capitalinicial, int periodo)
+        {
+            return  capitalinicial * Math.Pow((1 + ), periodo);
+        }
+    }
+}
---
+++ UcPag/UcPagTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UcPagTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
