6df3116c028ac5b16e4880247403cf040d1460fb
Francisco <francisco.amartins.al@gmail.com>
2019-11-14 22:28:52 -0300

    Enums

---
+++ Enum_One/Enum_One/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Enum_One.Entities.Enums
+{
+    enum OrderStatus : int // Identifica o tipo primitivo.
+    {
+        PedingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ Enum_One/Enum_One/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Enum_One.Entities;
+using Enum_One.Entities.Enums;
+
+namespace Enum_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PedingPayment
+            };
+
+            Console.WriteLine(order);
+
+
+            string txt = OrderStatus.PedingPayment.ToString(); // Convete o tipo enúmerado em string.
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered"); // Converte o tipo strins em tipo enúmerado.
+            Console.WriteLine(os);
+        }
+    }
+}
